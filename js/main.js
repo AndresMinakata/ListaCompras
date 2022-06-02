0efdae7cd2e8cc2960d6aa11d1bd779cd7729f55
Kazuo <kazuominakata@gmail.com>
2022-06-02 15:56:40 -0500

    Se agrego el contador de productos por pieza

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let contador = 0;
 let costoTotal = 0;
+let totalEnProductos = 0;
 
 
 // document.getElementById modifica etiqueta ID dentro del <body>
@@ -104,6 +105,8 @@ contador++;
 document.getElementById("contadorProductos").innerHTML = contador;
 let precio = (Math.floor((Math.random()*50)*100))/100;
 let cantidad = parseFloat(txtNumber.value);
+totalEnProductos+=(cantidad <1)? Math.ceil(cantidad):parseInt(cantidad);
+document.getElementById("productosTotal").innerHTML = totalEnProductos;
 costoTotal += (precio*cantidad);
 total.innerHTML = `$${costoTotal.toFixed(2)}`;
 
